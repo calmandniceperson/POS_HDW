b0ed05e98261f37207f3abf3ee32e701cd72ec8a
Michael Köppl <mko.mko31@gmail.com>
2016-09-26 13:40:31 +0200

    Apply formatting

--- 02_Casino/ccasino/src/main.c
+++ 02_Casino/ccasino/src/main.c
@@ -92,7 +92,7 @@ enum input_type get_input_type(const char *line_buf)
  * Fetches part stating actual amount of money from start budget string, copies
  * it to a separate array, converts it to int32_t and stores it in dest
  */
-void get_val_port(const char *line_buf, int32_t* dest, int32_t len)
+void get_val_port(const char *line_buf, int32_t *dest, int32_t len)
 {
 	char am_buf[sizeof(line_buf)-len];
 	memcpy(am_buf, &line_buf[len], sizeof(&am_buf)-1); 
@@ -110,7 +110,7 @@ char* get_curr_time()
 }
 
 /* Tries to withdraw amount from money_curr */
-void wdraw(int32_t* amount)
+void wdraw(int32_t *amount)
 {
 	money_curr -= *amount;
 	if (money_curr < 0) {
@@ -130,7 +130,7 @@ void wdraw(int32_t* amount)
 }
 
 /* Handles starting entries */
-void start(char* line_buf)
+void start(char *line_buf)
 {
 	get_val_port(line_buf, &money_curr, sizeof(START_BUDGET_STR));
 	printf("Start budget: %d Euros\n", money_curr);
@@ -139,7 +139,7 @@ void start(char* line_buf)
 }
 
 /* Handle guesses for even numbers */
-void even(char* line_buf, int *number, int *val)
+void even(char *line_buf, int *number, int *val)
 {
 	printf("Even guess: %s", line_buf);
 	get_val_port(line_buf, val, strlen("G "));
@@ -154,7 +154,7 @@ void even(char* line_buf, int *number, int *val)
 }
 
 /* Handle guesses for odd numbers */
-void odd(char* line_buf, int* number, int* val)
+void odd(char *line_buf, int *number, int *val)
 {
 	printf("Odd guess: %s", line_buf);
 	get_val_port(line_buf, val, strlen("U "));
@@ -169,7 +169,7 @@ void odd(char* line_buf, int* number, int* val)
 }
 
 /* Handles exact guesses for numbers */
-void exact(char* line_buf, int* number, int* val)
+void exact(char *line_buf, int *number, int *val)
 {
 	printf("Exact guess: %s", line_buf);
 	get_val_port(line_buf, val, 2);
@@ -201,7 +201,7 @@ void end()
  * Gets the players name from the given line buffer by removing the endline
  * character
  */
-void get_name(char* line_buf)
+void get_name(char *line_buf)
 {
 	/* remove \n */
 	p_name = malloc(sizeof(line_buf));
