53ef2ac5333507c3cf0d94981deae60d52b641cc
Michael Köppl <mko.mko31@gmail.com>
2017-03-22 09:25:19 +0100

    Remove unecessary logic

--- CinemaQueueSim/Escalator.cs
+++ CinemaQueueSim/Escalator.cs
@@ -6,21 +6,5 @@ namespace ConsoleApplication
     {
         public const int CAPACITY_PER_MINUTE = 4000/60;
         const int TOTAL_TIME = 1;
-
-        private static Escalator instance;
-
-        private Escalator() {}
-
-        public static Escalator Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new Escalator();
-                }
-                return instance;
-            }
-        }
     }
 }
